dde6fc1722a5f5f12f5e3da3ab854f488df40dd9
ludmilx <lulupsg92@gmail.com>
2023-02-01 16:22:00 +0100

    add: updates 2.0

--- main.sh
+++ main.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# finir gnuplot t3(=p3) et wind
+# vérifier pour les arguments si consignes bien respctées
+# comprendre pourquoi lors d'un test du style ./main.sh -f meteo_filtered_data_v1.csv  -o test.csv -h
+# le fichier csv test n'est pas créé et n'est pas trié par le C
+
 aide() {
   echo "-p : pression atmosphérique"
   echo "-t : température"
@@ -192,7 +197,8 @@ filtrage_coordonnees() {
 }
 
 filtrage_colonnes() {
-  cut -f "$2" -d ";" "coord_${1}_$sortie" > "colonne_${1}_$sortie"
+  #cut -f "$2" -d ";" "coord_${1}_$sortie" > "colonne_${1}_$sortie"
+  awk -F ";" '!seen[$1]++ {print $1 ";" $10 ";" $14}' "coord_${1}_$sortie" > "colonne_${1}_$sortie"
 }
 
 if [[ "$t" ]] #température
@@ -200,6 +206,34 @@ then
   filtrage_coordonnees "t$t"
   filtrage_colonnes "t$t" "1,2,11,12,13"
   ./app "colonne_t$t_$sortie" "$sortie" "t$t" "$tri" "$reverse"
+  if [[ "$t" = 1 ]]
+  then
+    gnuplot -persist -e "reset;set datafile separator \";\";
+      set title \"Température t1 par station\";
+      set xlabel \"ID de la station\";
+      set ylabel \"Température (°C)\";
+      set grid;
+      set key top left;
+      set style data linespoints;
+      set style fill solid 0.5;
+      set boxwidth 0.5;
+      plot '${sortie}' using 1:3:2:4 with yerrorbars title \"Moyenne\";
+      replot '${sortie}' using 1:2 with points title \"Minimum\";
+      replot '${sortie}' using 1:4 with points title \"Maximum\";"
+    elif [[ "$t" = 2 ]]
+    then
+      gnuplot -persist -e "reset;
+            set title 'Température moyenne sur le temps';
+            set xlabel 'Temps';
+            set ylabel 'Température (°C)';
+            set xdata time;
+            set timefmt '%Y-%m-%dT%H:%M:%S%z';
+            set datafile separator ';';
+            plot '$sortie' using 1:2 w l;"
+        ;;
+  else
+#######
+
 fi
 
 if [[ "$p" ]] #pression
@@ -207,7 +241,35 @@ then
   filtrage_coordonnees "p$p"
   filtrage_colonnes "p$p" "1,2,3,7"
   ./app "colonne_p$p_$sortie" "$sortie" "p$p" "$tri" "$reverse"
+  if [[ "$p" =1 ]]
+  then
+  gnuplot -persist -e "reset;set datafile separator \";\";
+    set title \"Pression p1 par station\";
+    set xlabel \"ID de la station\";
+    set ylabel \"Pression\";
+    set grid;
+    set key top left;
+    set style data linespoints;
+    set style fill solid 0.5;
+    set boxwidth 0.5;
+    plot '${sortie}' using 1:3:2:4 with yerrorbars title \"Moyenne\";
+    replot '${sortie}' using 1:2 with points title \"Minimum\";
+    replot '${sortie}' using 1:4 with points title \"Maximum\";"
+  elif [[ "$p" = 2 ]]
+  then
+    gnuplot -persist -e "reset;
+          set title 'Pression moyenne sur le temps';
+          set xlabel 'Temps';
+          set ylabel 'Pression';
+          set xdata time;
+          set timefmt '%Y-%m-%dT%H:%M:%S%z';
+          set datafile separator ';';
+          plot '$sortie' using 1:2 w l;"
+      ;;
+    else
+      ####
 fi
+
 
 if [[ "$w" ]] #vent
 then
@@ -219,13 +281,37 @@ fi
 if [[ "$h" ]] #altitude
 then
   filtrage_coordonnees "h"
-  filtrage_colonnes "h" "1,2,14"
-  ./app "colonne_h_$sortie" "$sortie" "h" "$tri" "$reverse"
+  filtrage_colonnes "h" "1,14,10"
+  ./app "colonne_h_$sortie" "/Users/ludmila/github/projetinfometeo/$sortie" "h" "$tri" "$reverse"
+  sed -i 's/,/;/g' "$sortie"
+  gnuplot -persist -e "reset;
+    set datafile separator \";\";
+    set title \"Altitude par station\";
+    set cblabel \"Altitude (m)\";
+    set xlabel \"Longitude\";
+    set ylabel \"Latitude\";
+    set xrange [-180:180];
+    set yrange [-90:90];
+    set dgrid3d 100,100;
+    set view map;
+    splot '$sortie' u 4:3:2 w pm3d lw 6 palette;"
 fi
 
 if [[ "$m" ]] #humidité
 then
   filtrage_coordonnees "m"
-  filtrage_colonnes "m" "1,2,6"
+  filtrage_colonnes "m" "1,6,10"
   ./app "colonne_m_$sortie" "$sortie" "m" "$tri" "$reverse"
+  sed -i 's/,/;/g' "$sortie"
+  gnuplot -persist -e "reset;
+    set datafile separator \";\";
+    set title \"Humidité par station\";
+    set cblabel \"Humidité\";
+    set xlabel \"Longitude\";
+    set ylabel \"Latitude\";
+    set xrange [-180:180];
+    set yrange [-90:90];
+    set dgrid3d 100,100;
+    set view map;
+    splot '$sortie' u 4:3:2 w pm3d lw 6 palette;"
 fi
